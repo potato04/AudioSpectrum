08ceeae19c7542fac29405a72513a20e7f724af8
potato04 <shiww@outlook.com>
2019-03-07 15:35:42 +0800

    replace placeholder with article url

--- AudioSpectrum02-starter/AudioSpectrum02/RealtimeAnalyzer.swift
+++ AudioSpectrum02-starter/AudioSpectrum02/RealtimeAnalyzer.swift
@@ -1,6 +1,6 @@
 //
 // AudioSpectrum02
-// A demo project for blog: https://juejin.im/timeline
+// A demo project for blog: https://juejin.im/post/5c1bbec66fb9a049cb18b64c
 // Created by: potato04 on 2019/1/30
 //
 
